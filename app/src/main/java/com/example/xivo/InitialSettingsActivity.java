1ae54e973f29777669374fb921ab720e81189a7f
Andres Vertel <avertel@gmail.com>
2024-10-13 17:15:18 -0500

    se agrega la interfaz de configuracion inicial en blanco

---
+++ app/src/main/java/com/example/xivo/InitialSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.xivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_settings);
+    }
+}
